c0e999ef151b14191827b4af63b2fbd2729b28de
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-13 16:17:45 +1000

    ...

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
         const items = ["Home", "About", "Projects", "Contact"];
 
     return (
-        <header className="sticky top-0 relative border-b border-solid z-20 bg-aliceblue">
+        <header className="sticky top-0 relative border-solid z-20 bg-aliceblue">
             <div className="max-w-[1200px] mx-auto w-full flex items-center justify-between px-8 lg:px-6 py-6 lg:py-4">
                 <a className="flex text-center font-medium sm:text-lg" href="">NataliaTR</a>
                 <nav className="sm:text-lg">
