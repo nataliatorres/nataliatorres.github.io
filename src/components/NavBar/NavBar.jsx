288478fce80e94030a3640ebf197c7599be795c7
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-18 15:57:02 +1000

    Adding the underlined effect

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,12 +3,15 @@ const NavBar = () => {
 
     return (
         <header className="top-0 relative border-solid z-20 bg-[#4F5030]">
-            <div className="max-w-[1200px] mx-auto w-full flex items-center justify-between px-8 lg:px-6 py-6 lg:py-4">
+            <div className="max-w-[1200px] mx-auto w-full flex items-center justify-between gap-4 duration-200 px-8 lg:px-6 py-6 lg:py-4">
                 <a className="flex text-center font-medium sm:text-lg" href="">Natalia Torres Rojas</a>
-                <nav className="sm:text-lg">
+                <nav className="sm:text-lg flex items-center gap-4 md:gap-5 lg:gap-2">
                 {
                     navItems.map((item, index) => (
-                        <a className="pr-[5px]" href={`#${item}`} key={index}>{item}</a>
+                        <a className="relative overflow-hidden group" href={`#${item.toLocaleLowerCase()}`} key={index}>
+                            {item}
+                            <div className="absolute bottom-0 right-full group-hover:translate-x-full duration-300 h-[1.5px] w-full bg-[#CA9248]"></div>
+                        </a>
                     ))
                 }
                 </nav>
