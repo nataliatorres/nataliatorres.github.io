f1987902e21b997b9a3d691849d36cfa6d0a4ea1
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-19 15:15:59 +1000

    Adding the hamburger icon and styling the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const NavBar = () => {
     const navItems = ["About", "Projects", "Contact"];
 
@@ -6,9 +8,10 @@ const NavBar = () => {
             <div className="max-w-[1200px] mx-auto w-full flex items-center justify-between gap-4 duration-200 px-8 lg:px-6 py-6 lg:py-4">
                 <a className="flex text-center font-medium sm:text-lg" href="">Natalia Torres Rojas</a>
                 <nav className="sm:text-lg flex items-center gap-4 md:gap-5 lg:gap-2">
+                    <a href="" className="md:hidden"><FontAwesomeIcon icon="fa-solid fa-bars" className="fa-2x"/></a>
                 {
                     navItems.map((item, index) => (
-                        <a className="relative overflow-hidden group" href={`#${item.toLocaleLowerCase()}`} key={index}>
+                        <a className="relative overflow-hidden hidden md:block group" href={`#${item.toLocaleLowerCase()}`} key={index}>
                             {item}
                             <div className="absolute bottom-0 right-full group-hover:translate-x-full duration-300 h-[1.5px] w-full bg-[#CA9248]"></div>
                         </a>
