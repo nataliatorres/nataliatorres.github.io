2dff177a71f5463e87fa08d8287d31b5bdaec997
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-19 15:16:42 +1000

    Changing the footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <footer className=" relative border-solid z-20 bg-[#4F5030] text-[#DFD3BD]">
             <div className="max-w-[1200px] mx-auto w-full flex flex-col items-center gap-1 px-8 lg:px-6 py-6 lg:py-4">
                 <p>Crafted in 🇦🇺</p>
-                <p>by a 🇨🇴❤️</p>
+                <p>with ❤️ by a 🇨🇴</p>
             </div>
         </footer>
     )
