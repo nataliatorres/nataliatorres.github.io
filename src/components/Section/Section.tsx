06f5b984739096e414b8a49b1401dbe74192e9f8
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-19 15:14:53 +1000

    Changing the text color and styling the avatar

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -8,14 +8,14 @@ type SectionProps = {
 const Section = ({ id, title, text, imgSource }: SectionProps) => {
 
     return (
-        <div id={id} className="flex justify-center flex-wrap-reverse max-w-300 mx-auto w-full p-12 lg:flex-nowrap lg:gap-14 text-black ">
+        <div id={id} className="flex justify-center flex-wrap-reverse max-w-300 mx-auto w-full p-12 lg:flex-nowrap lg:gap-14 text-[#4F5030] ">
             <div className="lg:content-center mt-2.5">
                 <h1>{title}</h1>
                 <p>{text}</p>
             </div>
             <div>
-                <figure>
-                    <img className="max-w-87.5 rounded-full" src={imgSource} alt={imgSource ? "The user avatar" : ""} />
+                <figure className="flex flex-wrap place-content-center">
+                    <img className="lg:max-w-78 max-w-[80%] rounded-full" src={imgSource} alt={imgSource ? "The user avatar" : ""} />
                 </figure>
             </div>
         </div>
