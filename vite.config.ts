03f8c3900178f58b8129c382123dc640d50ad88e
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-24 16:20:42 +1000

    Updating the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-
-  return config
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    tailwindcss(),
+  ],
+  base: "/"
 })
