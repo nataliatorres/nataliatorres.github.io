9248239ef4d4fac52eb7c5ebfaf38f6efcf581ef
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-03-08 17:33:52 +1000

    Add a Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,22 @@
+type LogoTag = 'img' | 'a'
+
+type LogoProps<T extends LogoTag> = {
+    type: LogoTag
+    styles: string
+    src: T extends 'img' ? string : never
+    alt: T extends 'img' ? string : never
+    href: T extends 'a' ? string : never
+    icon: T extends 'a' ? string : never
+}
+
+const Logo = <T extends LogoTag>({ type, styles, src, alt, href, icon }: LogoProps<T>) => {
+    return (
+        type === 'img' ? (
+            <img className={styles} src={src} alt={alt} />
+        ) : (
+        <a href={href} target="_blank"> <FontAwesomeIcon icon={icon} className={styles}/> </a>
+        )
+    )
+}
+
+export default Logo
