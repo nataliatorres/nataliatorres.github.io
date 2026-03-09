79c8ad45e20cf4418d4046af1daac09f963e063d
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-03-09 22:30:57 +1000

    Update the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,8 @@
 const Footer = () => {
   return (
     <footer className=' relative border-solid z-20 bg-[#4F5030] text-[#DFD3BD]'>
-      <div className='max-w-[1200px] mx-auto w-full flex flex-col items-center gap-1 px-8 lg:px-6 py-6 lg:py-4'>
-        <p>© 2026</p>
-        <p>Crafted in 🇦🇺</p>
-        <p>by a 🇨🇴❤️</p>
+      <div className='max-w-300 mx-auto w-full flex flex-col items-center gap-1 px-8 lg:px-6 py-6 lg:py-4'>
+        <p>© 2026 · Crafted in 🇦🇺 by a 🇨🇴 ❤️ </p>
       </div>
     </footer>
   )
