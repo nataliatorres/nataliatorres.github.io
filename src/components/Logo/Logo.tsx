1da866552c0fedba9cf8366eda546af31a4f1adf
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-05-26 15:17:38 +1000

    Update the Logo component. Use a factory pattern to pick the right component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,26 +2,54 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 type LogoTag = 'img' | 'a' | 'icon'
 
-type LogoProps<T extends LogoTag> = {
-    type: LogoTag
+type ImgLogoProps = {
+    src: string
+    alt: string
+    loading: string
     styles: string
-    src: T extends 'img' ? string : never
-    alt: T extends 'img' ? string : never
-    loading: T extends 'img' ? string : never
-    href: T extends 'a' ? string : never
-    icon: T extends 'a' | 'icon' ? string : never
 }
 
-const Logo = <T extends LogoTag>({ type, styles, src, alt, loading, href, icon }: LogoProps<T>) => {
-    return (
-        type === 'img' ? (
-            <img className={styles} src={src} alt={alt} loading={loading}/>
-        ) : type === 'icon' ? (
-            <FontAwesomeIcon icon={icon} className={styles} />
-        ) : (
-        <a href={href} target="_blank"> <FontAwesomeIcon icon={icon} className={styles}/> </a>
-        )
-    )
+const ImgLogo = ({ src, alt, loading, styles }: ImgLogoProps) => (
+    <img className={styles} src={src} alt={alt} loading={loading}/>
+)
+
+type IconLogoProps = {
+    icon: string
+    styles: string
+}
+
+const IconLogo = ({ icon, styles }: IconLogoProps) => (
+    <FontAwesomeIcon icon={icon} className={styles} />
+)
+
+type LinkLogoProps = {
+    href: string
+    icon: string
+    styles: string
+}
+
+const LinkLogo = ({ href, icon,styles }: LinkLogoProps) => (
+    <a href={href} target="_blank">
+        <FontAwesomeIcon icon={icon} className={styles}/>
+    </a>
+)
+
+type LogoType = 'img' | 'icon' | 'link'
+
+type LogoProps =
+| ({ type: 'img' } & ImgLogoProps)
+| ({ type: 'icon' } & IconLogoProps)
+| ({ type: 'link' } & LinkLogoProps)
+
+const Logo = (props: LogoProps) => {
+    switch (props.type) {
+        case 'img':
+            return <ImgLogo src={props.src} alt={props.alt} loading={props.loading} styles ={props.styles} />
+        case 'icon':
+            return <IconLogo icon={props.icon} styles={props.styles} />
+        case 'link':
+            return <LinkLogo href={props.href} icon={props.icon} styles={props.styles} />
+    }
 }
 
 export default Logo
